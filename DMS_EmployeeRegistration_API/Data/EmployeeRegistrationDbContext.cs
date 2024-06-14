bf9fed96a8d2009200aef04dae8226fa9f1b9565
Diego Santos <diegom.santos03@gmail.com>
2024-06-13 22:18:43 -0300

    (chore): finalizado projeto de uma api com mvc

---
+++ DMS_EmployeeRegistration_API/Data/EmployeeRegistrationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DMS_EmployeeRegistration_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMS_EmployeeRegistration_API.Data
+{
+    public class EmployeeRegistrationDbContext : DbContext
+    {
+        public EmployeeRegistrationDbContext (DbContextOptions<EmployeeRegistrationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; } = default!;
+    }
+}
---
+++ DMS_EmployeeRegistration_API/Migrations/20240614010700_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMS_EmployeeRegistration_API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Function = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
